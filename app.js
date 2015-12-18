280254815efa71e2bce542d272561642786b7c37
Tom McKenzie <tom@chillidonut.com>
2015-12-18 23:08:07 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+class Playlist {
+  constructor() {
+    console.log('init, connecting to stpodyi...');
+
+  }
+}
+
+
+function app() {
+  let cool_sync = new Playlist();
+}
+
+
+export default app;
+
---
+++ index.js
@@ -0,0 +1,8 @@
+require("babel-register");
+
+// load your app
+var app = require("./app.js");
+
+app.default();
+
+console.log('we\'re done 💯');
